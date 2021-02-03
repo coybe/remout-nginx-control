5b59fa4d5aa947edabfd441b96174384b77e6536
coybe <coybe_cao@163.com>
2021-02-03 14:27:33 +0800

    Revert "Create upload and restart services.bat"
    This reverts commit 848e438e9386d8ca017d48e00625f9c5b5746806.

--- upload and restart services.bat
+++
@@ -1,7 +0,0 @@
-REM 声明采用UTF-8编码
-chcp 65001
-scp C:\Users\Admin\Desktop\deploy\V1.5\*.jar root@119.29.146.227:/mnt/skynet/deploy/jarupload
-
-ssh root@119.29.146.227  "/mnt/skynet/deploy/bat_jarservice.sh restart all"
-
-pause
